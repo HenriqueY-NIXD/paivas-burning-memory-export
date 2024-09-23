4ac305f06d0a45c0017a76ffad8e654e0b4ecc89
Dev 15 <henrique.yamada@gbmlogistica.com.br>
2024-09-22 23:06:33 -0300

    front finished

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,25 @@
+<script lang="ts">
+    import { invoke } from '@tauri-apps/api/tauri';
+    import { open } from '@tauri-apps/api/dialog';
+
+    async function sporta() {
+        const folderPath = await open({directory: true});
+        await invoke('sporta', folderPath)
+    }
+</script>
+
 <main>
   <div id="main">
-    <button>Sporta</button>
+    <button on:click="{sporta}">Sporta</button>
     <div id="img"></div>
   </div>
 </main>
 
 <style>
     main {
-        width: 1280px;
-        height: 720px;
+        width: 1200px;
+        height: 700px;
+        display: flex;
         flex: 1;
         justify-content: center;
         align-items: center;
@@ -21,7 +32,15 @@
         box-shadow: none;
         cursor: pointer;
         width: 500px;
-        height: 250px;
+        height: 100px;
+        font-size: larger;
+        font-weight: bolder;
+        color: #fff;
+        border: solid black 1px;
+    }
+
+    button:hover {
+        background: #414141;
     }
 
     button:active {
@@ -35,5 +54,7 @@
         border: solid 1px #fff;
         height: 500px;
         width: 500px;
+        background-image: url("/god.png");
+        background-size: 500px 500px;
     }
 </style>
