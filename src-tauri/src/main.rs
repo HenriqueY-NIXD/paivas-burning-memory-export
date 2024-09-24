f47ad33b2a85c4e5f7a497eb1986b1b5c2e37c24
Dev 15 <henrique.yamada@gbmlogistica.com.br>
2024-09-23 23:55:06 -0300

    sucess, error, RUST and ENV

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,9 +1,80 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use sqlx::{postgres::PgPoolOptions, Error};
+use chrono::{FixedOffset, DateTime, NaiveDate};
+use url::Url;
+use serde::Serialize;
+use std::env;
+
+#[derive(Serialize)]
+struct AlbumListen {
+    order: i32,
+    album: String,
+    listen_at: Option<NaiveDate>,
+    created_at: DateTime<FixedOffset>,
+    photo: Option<String>,
+    artist_name: String,
+}
+
 #[tauri::command]
-fn sporta(folder_path: String) {
-    
+async fn sporta() -> Result<Vec<AlbumListen>, ()>{
+    let pass = env::var("PASS").expect("PASS not set");
+    let user = env::var("USER").expect("USER not set");
+    let host = env::var("HOST").expect("HOST not set");
+    let database = env::var("DB").expect("DB not set");
+    let port = env::var("PORT").expect("PORT not set");
+
+    let db_uri = format!("postgres://{}:{}/{}", host, port, database);
+
+    let mut uri = Url::parse(&db_uri).unwrap();
+    let _ = uri.set_username(user.as_str());
+    let _ = uri.set_password(Some(pass.as_str()));
+    let uri = uri.as_str();
+    let pool = PgPoolOptions::new()
+        .max_connections(1)
+        .connect(uri).await;
+
+    match pool {
+        Ok(pool) => { 
+            let row: Result<Vec<(i32, String, Option<NaiveDate>, DateTime<FixedOffset>, Option<String>, String)>, Error> = 
+                sqlx::query_as(" \
+                    SELECT \
+                        al.\"order\", al.album, al.listen_at, al.created_at, al.photo, a.\"name\" \
+                    FROM album_listen al
+                        JOIN artist a ON (al.artist_id=a.id); \
+                ").fetch_all(&pool).await;
+
+            match row {
+                Ok(data) => {
+                    let mut result: Vec<AlbumListen> = Vec::new();
+
+                    for row in data {
+                        result.push(AlbumListen {
+                            order: row.0,
+                            album: row.1,
+                            listen_at: row.2,
+                            created_at: row.3,
+                            photo: row.4,
+                            artist_name: row.5
+                        });
+                    }
+
+                    pool.close().await;
+
+                    return Ok(result);
+                },
+                Err(err) => { 
+                    println!("{:?}", err);
+                    return Err(()); 
+                }        
+            }
+        },
+        Err(err) => { 
+            println!("{:?}", err);
+            return Err(()); 
+        }
+    }
 }
 
 fn main() {
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,38 @@
-<script lang="ts">
+<script lang="js">
     import { invoke } from '@tauri-apps/api/tauri';
     import { open } from '@tauri-apps/api/dialog';
 
+    // type Data = {
+    //     album: string;
+    //     artist_name: string;
+    //     created_at: string;
+    //     listen_at: string;
+    //     order: number;
+    //     photo: string;
+    // }
+
+    let isError = false;
+    let isSucess = false;
+
     async function sporta() {
         const folderPath = await open({directory: true});
-        await invoke('sporta', folderPath)
+        if (!folderPath) return;
+
+        invoke('sporta')
+        .then((data) => {
+            isSucess = true;
+
+            setTimeout(() => {
+                isSucess = false;
+            }, 2000);
+        })
+        .catch(() => {
+            isError = true;
+
+            setTimeout(() => {
+                isError = false;
+            }, 2000);
+        });
     }
 </script>
 
@@ -15,7 +43,39 @@
   </div>
 </main>
 
+{#if isError && !isSucess}
+<div id="erro" class="toast">Não foi possível salvar o arquivo!</div>
+{/if}
+
+{#if !isError && isSucess}
+<div id="sucesso" class="toast">Arquivo Salvo!</div>
+{/if}
+
 <style>
+    #sucesso {
+        background-color: #1fe419;
+    }
+
+    #erro {
+        background-color: #ff3434;
+    }
+
+    .toast {
+        min-width: 250px;
+        margin-left: -125px;
+        color: #fff;
+        text-align: center;
+        border-radius: 2px;
+        padding: 16px;
+        position: fixed;
+        z-index: 1;
+        right: 2%;
+        bottom: 30px;
+        font-size: 17px;
+        -webkit-animation: fadein 0.5s, fadeout 0.5s 2.5s;
+        animation: fadein 0.5s, fadeout 0.5s 2.5s;
+    }
+
     main {
         width: 1200px;
         height: 700px;
